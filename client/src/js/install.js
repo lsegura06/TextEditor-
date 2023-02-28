3bf47616eecf5bb93824fa21ca0f26f9cad76174
lsegura06 <leosegura006@gmail.com>
2023-02-27 22:52:02 -0700

    still working on db logic

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,38 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+
+    // Store the event for later use
+    deferredPrompt = event;
+
+    // Show the install button
+    butInstall.style.display = 'block';
+});
+
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // Hide the install button
+    butInstall.style.display = 'none';
+
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+
+    if (outcome === 'accepted') {
+    } else {
+        console.log('PWA installation rejected');
+    }
+
+    // Clear the deferredPrompt variable
+    deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => { 
+    window.deferredPrompt = null;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,11 @@
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,13 +19,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main']
+      }),
+      new WebpackPwaManifest({
+        name: 'My App',
+        short_name: 'My App',
+        description: 'My awesome app',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        exclude: [/\.map$/, /asset-manifest\.json$/],
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
 };
+
+
